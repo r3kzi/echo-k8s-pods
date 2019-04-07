dc4d36afec022fac53bb6c094e98cb5653a2f969
Christian Niehoff <christian.niehoff@dwpbank.de>
2019-04-07 17:06:46 +0200

    hello http echo

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
-# Example Pods for Kubernetes
+# Echo Pods for Kubernetes
 
-## TCP Server
+## tcp
 
 ```bash
 $ docker run -p 9000:9000 rekzi/tcp-echo -prefix one
 listening on port [::]:9000 with prefix: one
 ```
-
-```bash
-$ docker run -p 9001:9000 rekzi/tcp-echo -prefix two
-listening on port [::]:9000 with prefix: two
-```
-
 ```bash
 $ date | nc localhost 9000
 one Sa 6. Apr 09:59:27 CEST 2019
+```
 
-$ date | nc localhost 9001
-two Sa 6. Apr 10:00:42 CEST 2019
+## http
+
+```bash
+docker run -p 8080:8080 rekzi/http-echo -version v1                                                                      7.3s  So 07 Apr 2019 17:01:18 CEST
+2019/04/07 15:04:45 Server is starting...
+2019/04/07 15:04:45 Server is ready to handle requests at :8080 
+```
+
+```bash
+$ curl localhost:8080
+v1
 ```
---
+++ http-echo/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var (
+	listenAddr string
+	version    string
+)
+
+func main() {
+	flag.StringVar(&listenAddr,"listen-adr", "8080", "server listen address")
+	flag.StringVar(&version,"version", "v1", "version")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+	logger.Println("Server is starting...")
+
+	router := http.NewServeMux()
+	router.Handle("/", handler(version))
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%v", listenAddr),
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	done := make(chan bool)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		logger.Println("Server is shutting down...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			logger.Fatalf("Could not gracefully shutdown the server: %v\n", err)
+		}
+		close(done)
+	}()
+
+	logger.Printf("Server is ready to handle requests at :%s \n", listenAddr)
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatalf("Could not listen on %s: %v\n", listenAddr, err)
+	}
+
+	<-done
+	logger.Println("Server stopped")
+}
+
+func handler(version string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, version)
+	})
+}
